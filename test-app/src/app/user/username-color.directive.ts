6f49c0320d8a4b4fd277019e31d6ac5f49766e2e
Shrsharma <shrsharma@teksystems.com>
2021-03-25 17:18:33 +0530

    commit

--- test-app/src/app/user/username-color.directive.ts
+++ test-app/src/app/user/username-color.directive.ts
@@ -1,12 +1,12 @@
 import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[ ]'
+  selector: '[userColor]'
 })
 export class UsernameColorDirective {
 
   constructor(element: ElementRef, renderer: Renderer2) {
-    renderer.setStyle(element.nativeElement, 'color','red');
-   }
+    renderer.setStyle(element.nativeElement, 'color', 'red');
+  }
 
 }
